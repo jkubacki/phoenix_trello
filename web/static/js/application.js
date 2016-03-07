1ee502d7bd34af63e384dda1b360aea8076a0a86
Jakub Kubacki <kubacki.jk@gmail.com>
2016-03-07 01:27:46 +0100

    Drop jsx for now

--- web/static/js/application.js
+++ web/static/js/application.js
@@ -2,7 +2,7 @@ import React                    from 'react';
 import ReactDOM                 from 'react-dom';
 import { browserHistory }       from 'react-router';
 import configureStore           from './store';
-import Root                     from './containers/root.jsx';
+import Root                     from './containers/root';
 
 const store  = configureStore(browserHistory);
 
