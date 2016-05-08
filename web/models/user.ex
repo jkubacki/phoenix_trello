b7746236fecbf627c061e310dd40f10ac3b70cd2
Jakub Kubacki <kubacki.jk@gmail.com>
2016-05-08 18:29:32 +0200

    Add validations to user model

--- web/models/user.ex
+++ web/models/user.ex
@@ -10,8 +10,8 @@ defmodule PhoenixTrello.User do
     timestamps
   end
 
-  @required_fields ~w(first_name last_name email encrypted_password)
-  @optional_fields ~w()
+  @required_fields ~w(first_name last_name email)
+  @optional_fields ~w(encrypted_password)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -22,5 +22,9 @@ defmodule PhoenixTrello.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, message: "Password does not match")
+    |> unique_constraint(:email, message: "Email already taken")
   end
 end
