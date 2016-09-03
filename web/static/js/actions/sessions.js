6a2cdf6cef733b8b35a6971c5b32e006498e0765
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-03 17:19:48 +0200

    Add current user action

--- web/static/js/actions/sessions.js
+++ web/static/js/actions/sessions.js
@@ -40,7 +40,20 @@ const Actions = {
     };
   },
 
-  // ...
+  currentUser: () => {
+    return dispatch => {
+      const authToken = localStorage.getItem('phoenixAuthToken');
+
+      httpGet('/api/v1/current_user')
+      .then(function (data) {
+        setCurrentUser(dispatch, data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        dispatch(push('/sign_in'));
+      });
+    };
+  }
 };
 
 export default Actions;
