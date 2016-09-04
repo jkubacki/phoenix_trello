4f5bee26136743168a19f1f734ee82ee1a19c01a
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-04 22:34:52 +0200

    Add boards reducers to main reducer

--- web/static/js/reducers/index.js
+++ web/static/js/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers }  from 'redux';
 import { routeReducer }     from 'react-router-redux';
 import session              from './session';
 import registration         from './registration';
+import boards               from './boards';
 
 export default combineReducers({
   routing: routeReducer,
   session: session,
-  registration: registration
+  registration: registration,
+  boards: boards,
 });
