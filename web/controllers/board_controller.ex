7534ec8657e797f4f1f227183d1d3c6de24dc82f
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-05 01:38:46 +0200

    Preload user on board create

--- web/controllers/board_controller.ex
+++ web/controllers/board_controller.ex
@@ -21,6 +21,7 @@ defmodule PhoenixTrello.BoardController do
 
     changeset = current_user
       |> build_assoc(:owned_boards)
+      |> Repo.preload(:user)
       |> Board.changeset(board_params)
 
     if changeset.valid? do
