e473f64b235bc09e317311f8d754c976d66fa76e
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-06 22:31:21 +0200

    Create list in board channel

--- web/channels/board_channel.ex
+++ web/channels/board_channel.ex
@@ -20,6 +20,24 @@ defmodule PhoenixTrello.BoardChannel do
     {:noreply, socket}
   end
 
+  def handle_in("lists:create", %{"list" => list_params}, socket) do
+    board = socket.assigns.board
+
+    changeset = board
+      |> build_assoc(:lists)
+      |> List.changeset(list_params)
+
+    case Repo.insert(changeset) do
+      {:ok, list} ->
+        list = Repo.preload(list, [:board, :cards])
+
+        broadcast! socket, "list:created", %{list: list}
+        {:noreply, socket}
+      {:error, _changeset} ->
+        {:reply, {:error, %{error: "Error creating list"}}, socket}
+    end
+  end
+
   def terminate(_reason, socket) do
     board_id = socket.assigns.board.id
     user_id = socket.assigns.current_user.id
